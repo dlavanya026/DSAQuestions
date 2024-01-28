faaffad6762abd054b7e7aed4b67cd95808a06a9
dlavanya026 <dlavanya026@gmail.com>
2024-01-27 23:19:01 -0500

    Adding initial binary search question 7

--- BinarySearchQuestions/BasicBinarySearch_1.java
+++ BinarySearchQuestions/BasicBinarySearch_1.java
@@ -3,21 +3,25 @@ package BinarySearchQuestions;
 public class BasicBinarySearch_1 {
     public static void main(String[] args) {
         int[] arr={10,15,18,25,36,45,89};
-        int target=45;
-        int indexOfTarget= binarySearch(arr,target);
-        System.out.println(indexOfTarget);
+        int[] arr1 = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+
+        int target=22;
+        int indexOfTarget= binarySearch(arr1,target);
+        System.out.println(arr1[indexOfTarget]);
     }
+
 
     private static int binarySearch(int[] arr, int target) {
         int start=0;
         int end=arr.length-1;
         while(start<=end)
         {
-            int mid=end-(end-start)/2;
-            if(arr[mid]>target){
-                end=mid-1;
-            }else if(arr[mid]<target){
+            int mid=start+(end-start)/2;
+            if(target>arr[mid]){
                 start=mid+1;
+            }
+            else if(target<arr[mid]){
+                end=mid-1;
             }else{
                 return mid;
             }
